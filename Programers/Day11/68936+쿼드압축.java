6e9c64316189e5fabf4f82613ca1c7336266ab1e
gombbang <87199607+gombbang@users.noreply.github.com>
2023-09-03 17:58:57 +0900

    Update 68936+쿼드압축.java

--- Programers/Day11/68936+쿼드압축.java
+++ Programers/Day11/68936+쿼드압축.java
@@ -16,35 +16,21 @@ class Solution {
         
         for (int i = startX ; i < endX ; i++) {
             for (int j = startY; j < endY ; j++) {
-                // System.out.print(arr[i][j]);
                 if (arr[i][j] == 0)
                     answer[0] = answer[0]+1;
                 else
                     answer[1] = answer[1]+1;
             }
-            // System.out.println();
         }
             if (answer[0] == 0) {
                 answer[1] = 1;
-                
-                // System.out.println("special, answer[0] : " + answer[0] + "answer[1] :" + answer[1]);
                 return answer;
             } else if (answer[1] == 0) {
                 answer[0] = 1;
-                // System.out.println("special, answer[0] : " + answer[0] + "answer[1] :" + answer[1]);
-            
                 return answer;
             }
-        
-        
-            
-        // System.out.println("endX-startX :" + (endX-startX));
-        // System.out.println("endY-startY :" + (endY-startY));
-        
         // 나눠질 수 없음
         if (endX-startX == 2) {
-            // System.out.println("answer[0] : " + answer[0] + "answer[1] :" + answer[1]);
-            
             return answer;
         }
         else {
@@ -57,26 +43,12 @@ class Solution {
             int[] s4 = sol(midX,midY,endX,endY);
             answer[0] = s1[0] + s2[0] + s3[0] +s4[0];
             answer[1] = s1[1] + s2[1] + s3[1] +s4[1];
-            // System.out.println("answer[0] : " + answer[0] + "answer[1] :" + answer[1]);
             return answer;
         }
     }
     
     public int[] solution(int[][] a) {
         arr = a;
-        // System.out.println("a.length :" + a.length);
-        // System.out.println("a[0].length :" + a[0].length);
-        // for (int i = 0 ; i < a.length ; i++) {
-        //     for (int j = 0; j < a.length ; j++) {
-        //         System.out.print(a[i][j]);
-        //         // if (arr[i][j] == 0)
-        //         //     answer[0] = answer[0]+1;
-        //         // else
-        //         //     answer[1] = answer[1]+1;
-        //     }
-        //         System.out.println();
-        // }
-        // return null;
         return sol(0,0,a.length,a[0].length);
     }
 }
